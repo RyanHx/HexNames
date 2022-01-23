e1c681ceb10e5b807f42dbb27c019ec7344882a5
RyanHx <hollick5@hotmail.com>
2022-01-23 00:59:46 +0000

    Clear cin buffer, refactor to more appropriate variable names/types

--- HexNames.cpp
+++ HexNames.cpp
@@ -7,23 +7,23 @@
 
 namespace fs = std::filesystem;
 
-std::string extract_name(const std::string path, uint32_t offset)
+std::string extract_name(const fs::path path, uint32_t offset)
 {
 	std::vector<unsigned char> bytes;
-	std::ifstream input;
+	std::ifstream file_stream;
 
-	input.open(path, std::ios::binary);
-	if (input.fail())
+	file_stream.open(path, std::ios::binary);
+	if (file_stream.fail())
 	{
 		std::cout << "Failed to open " << path << '\n';
 		return "";
 	}
-	input.seekg(offset, std::ios::beg);
-	while (!input.eof())
+	file_stream.seekg(offset, std::ios::beg);
+	while (!file_stream.eof())
 	{
 		unsigned char byte;
-		input >> byte;
-		if (input.fail())
+		file_stream >> byte;
+		if (file_stream.fail())
 		{
 			return "";
 		}
@@ -33,36 +33,35 @@ std::string extract_name(const std::string path, uint32_t offset)
 		}
 		bytes.push_back(byte);
 	}
-	input.close();
+	file_stream.close();
 
 	return std::string(bytes.begin(), bytes.end());
 }
 
-std::string replace_with_extract(const std::string path, const std::string name)
+fs::path update_path(fs::path path, const std::string name)
 {
-	fs::path file_path{ path };
-	std::string ext{ file_path.extension().string() };
-	std::string new_path{ file_path.replace_filename(name).string() };
-	return new_path + ext;
+	return path.replace_filename(name).string() + path.extension().string();
 }
 
 int main(int argc, char* argv[])
-{	
+{
 	uint32_t offset;
 	std::cout << "Enter offset: ";
 	std::cin >> std::hex >> offset;
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 	for (int i{ 1 }; i < argc; ++i)
 	{
-		if (fs::exists(argv[i]))
+		fs::path file_path{ argv[i] };
+		if (fs::exists(file_path))
 		{
-			std::string extracted_name{ extract_name(argv[i], offset) };
+			std::string extracted_name{ extract_name(file_path, offset) };
 			if (extracted_name.empty())
 			{
-				std::cout << "Failed to find name for " << argv[i] << '\n';
+				std::cout << "Failed to find name for " << file_path.string() << '\n';
 				continue;
 			}
-			fs::rename(argv[i], replace_with_extract(argv[i], extracted_name));
-		}		
+			fs::rename(file_path, update_path(file_path, extracted_name));
+		}
 	}
 	std::cout << "Operation finished." << '\n';
 	static_cast<void>(getchar());
